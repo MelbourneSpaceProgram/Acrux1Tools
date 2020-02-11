ede328d6cddbb79bc9b22ed350b613b3b3047987
Ryan Crosby <ryan.crosby@live.com>
2020-02-11 16:17:37 +1100

    Add cache awareness to GetAllObservations and improve efficiency

--- SatnogsApi/ISatnogsNetworkApiExtensions.cs
+++ SatnogsApi/ISatnogsNetworkApiExtensions.cs
@@ -9,24 +9,38 @@ namespace SatnogsApi
 {
     public static class ISatnogsNetworkApiExtensions
     {
-        public static async Task<List<ObservationEntry>> GetAllObservations(this ISatnogsNetworkApi api, int satelliteId, int maxPages = 1024, int concurrentPages = 64)
+        public static async Task<List<ObservationEntry>> GetAllObservations(this ISatnogsNetworkApi api, int satelliteId, IEnumerable<ObservationEntry> previousObservations = null, int maxPages = 1024, int concurrentPages = 64)
         {
             // There are two ways to retrieve all observations:
             //
             // * One at a time.
             //   After each observation is fetched, the "link" header can be checked for a rel= "next" link which will give the next page.
+            //
             //   This will give all pages until the last page is reached, and no rel= "next" link is provided.
             //   This is the "cleanest" method supported by the API.
+            //
             //
             // * Multithreaded
             //   Attempt to get an entire batch of observations, all simultaneously at once.
             //   Continue grabbing entire batches until we hit a 404 Not Found (or we could technically check for missing rel= "next" link but doesn't make much difference).
+            //   We can also save a cache of previous Observation Ids, and then stop as soon as we hit one (since we've "caught up" to the latest data").
+            //
             //   This is fast and it works, but it's technically not a supported way to use the API.
             //
+            //
             // We use Multithreaded because it's so, so much quicker.
-            // 
+            //
 
-            List<ObservationEntry> observations = new List<ObservationEntry>();
+            Dictionary<long, ObservationEntry> observations = new Dictionary<long, ObservationEntry>();
+            HashSet<long> previousObservationIds = previousObservations == null ? null : new HashSet<long>(previousObservations.Select(oe => oe.Id));
+
+            if (previousObservations != null)
+            {
+                foreach (var thisPreviousObservation in previousObservations)
+                {
+                    observations[thisPreviousObservation.Id] = thisPreviousObservation;
+                }
+            }
 
             for (int i = 1; i < maxPages; i += concurrentPages)
             {
@@ -44,8 +58,25 @@ namespace SatnogsApi
                 {
                     try
                     {
-                        List<ObservationEntry> telemetryPage = await task;
-                        observations.AddRange(telemetryPage);
+                        List<ObservationEntry> observationsPage = await task;
+                        foreach (var thisObservation in observationsPage)
+                        {
+                            observations[thisObservation.Id] = thisObservation;
+                        }
+
+                        if (previousObservationIds != null)
+                        {
+                            // Check cache for observation.
+                            // If we hit one, it indicates that we have "caught up" to the previous cache.
+                            foreach (var observation in observationsPage)
+                            {
+                                if (previousObservationIds.Contains(observation.Id))
+                                {
+                                    end = true;
+                                    break;
+                                }
+                            }
+                        }
                     }
                     catch (Exception ex) when (ex.Message.Contains("404"))
                     {
@@ -59,8 +90,7 @@ namespace SatnogsApi
                 }
             }
 
-            // Remove duplicates from the observation list and return
-            return observations.GroupBy(t => t.Id).Select(g => g.FirstOrDefault()).Where(t => t != null).ToList();
+            return observations.Values.OrderBy(oe => oe.Id).ToList();
         }
     }
 }
